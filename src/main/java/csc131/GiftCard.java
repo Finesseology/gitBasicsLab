9f17b8fe751555d0a1b1f6645fa59e9ddbc50529
Chris Lemke <cwlemke@live.com>
2021-10-10 12:24:05 -0700

    Windows 10, changed deduction method to give more descriptive error if amount is not enough.

--- src/main/java/csc131/GiftCard.java
+++ src/main/java/csc131/GiftCard.java
@@ -63,7 +63,7 @@ public class GiftCard
         
         if (amount < 0.0)
         {
-            result = "Invalid Transaction";
+            result = "The amount on the card is not enough for this transaction.";
         }
         else
         {
